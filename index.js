4dfd528dc870b1abf1c1246f6d839410a7aa2a1e
Travis <tjwokasch@gmail.com>
2021-07-01 09:05:29 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solutions in this file
+let count = 10
+function countdown() {
+    let i = 10;
+    while (i >= 0){
+        console.log(i);
+        i--;
+    }
+
+    return count;
+}
+countdown();

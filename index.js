9b46ff23aeeabad295cba0fa9709e919d29289c5
Travis <tjwokasch@gmail.com>
2021-07-14 14:12:35 -0600

    commit tree 7fedd2dea74d2a4f184d31cafad5367df3b90f16

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-let count = 10
-function countdown() {
-    let i = 10;
+for (let age = 30; age < 40; age++){
+console.log(`I am ${age} years old. Happy birthday to me!`);
+}
+
+//const names = ['Lisa', 'Kaitlin', 'Jan'];
+
+function writeCards(names, event){
+    // Create empty array
+    let message = []
+    // Loop over array, add message to arryay
+    for (let i = 0; i < names.length; i++){
+       message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    // Return array
+    return message
+}
+
+//write function countDown
+function countDown(){
+   let i = 10
     while (i >= 0){
         console.log(i);
-        i--;
+        i--
     }
-
-    return count;
 }
-countdown();
+//takes in positive integer and counts down to zero using console.log()
+
+//countDown(10) actually logs 11 times
